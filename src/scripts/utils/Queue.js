b8afc1a5fa6d5dde06a62496312056cc1591e368
Nikita Latkovich <nicklatkovich@gmail.com>
2016-07-15 13:16:08 +0300

    added Queue

---
+++ src/scripts/utils/Queue.js
@@ -0,0 +1,36 @@
+/**
+ * Created by NickLatkovich on 15.07.2016.
+ */
+
+function QueueElement(data) {
+    this.data = data;
+    this.next = null;
+}
+
+function Queue() {
+    this.head = this.last = null;
+
+    this.pop = function () {
+        if (this.head) {
+            var result = this.head.data;
+            this.head = this.head.next;
+            if (this.head == null) {
+                this.last = null;
+            }
+            return result;
+        }
+        else {
+            return null;
+        }
+    };
+
+    this.push = function(data) {
+        var newElement = new QueueElement(data);
+        if (this.last) {
+            this.last = this.last.next = newElement;
+        }
+        else {
+            this.head = this.last = newElement;
+        }
+    }
+}
